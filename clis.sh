96d305189104c6e6d80c41976585d7c2e92632af
Pierre Fenoll <pierrefenoll@gmail.com>
2023-11-12 21:06:21 +0100

    fusion
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- clis.sh
+++ clis.sh
@@ -197,8 +197,7 @@ EOF
 if [[ $# = 0 ]]; then
   header
 
-  total=${#nvs[@]}
-  for ((i=0; i<total; i++)); do
+  for i in "${!nvs[@]}"; do
     name_at_version=${nvs["$i"]}
     cli "$name_at_version" "${nvs_args["$i"]}"
   done
@@ -206,3 +205,70 @@ if [[ $# = 0 ]]; then
   exit
 fi
 
+
+name_at_version=$1; shift
+cleanup=${1:-0}
+
+picked=-1
+for i in "${!nvs[@]}"; do
+  case "${nvs[$i]}" in
+    *"$name_at_version"*) picked=$i; break ;;
+  esac
+done
+if [[ "$picked" = -1 ]]; then
+  echo "Could not match '$name_at_version'"
+  for i in "${!nvs[@]}"; do
+    echo "${nvs[$i]}" "${nvs_args[$i]}"
+  done
+  exit 1
+fi
+name_at_version=${nvs[$i]}
+args=${nvs_args[$i]}
+
+session_name=$(sed 's%@%_%g;s%\.%-%g' <<<"$name_at_version")
+tmptrgt=/tmp/$session_name
+tmplogs=/tmp/$session_name.logs.txt
+tmpgooo=/tmp/$session_name.ready
+
+
+tmux new-session -d -s "$session_name"
+tmux select-window -t "$session_name:0"
+
+send() {
+  tmux send-keys "$* && exit" C-m
+}
+
+
+gitdir=$(realpath "$(dirname "$0")")
+send "CARGO_TARGET_DIR=/tmp/rstcbldx cargo install --locked --force --path=$gitdir"
+tmux split-window
+if [[ "$cleanup" = '1' ]]; then
+  send rm -rf "$tmptrgt"
+  tmux select-layout even-vertical
+  tmux split-window
+  send docker buildx prune -af '&&' touch "$tmpgooo"
+  tmux select-layout even-vertical
+  tmux split-window
+else
+  touch "$tmpgooo"
+fi
+
+send rustcbuildx pull
+tmux select-layout even-vertical
+tmux split-window
+
+
+send "rm $tmplogs; touch $tmplogs; tail -f $tmplogs; :"
+tmux select-layout even-vertical
+tmux split-window
+
+send \
+  'until' '[[' -f "$tmpgooo" ']];' 'do' sleep '1;' 'done' '&&' rm "$tmpgooo" '&&' \
+  RUSTCBUILDX_LOG=debug \
+  RUSTCBUILDX_LOG_PATH="$tmplogs" \
+  RUSTC_WRAPPER=rustcbuildx \
+    CARGO_TARGET_DIR="$tmptrgt" cargo -vv install --jobs=1 --locked --force "$name_at_version" "$args" \
+  '&&' tmux kill-session -t "$session_name"
+tmux select-layout even-vertical
+
+tmux attach-session -t "$session_name"
