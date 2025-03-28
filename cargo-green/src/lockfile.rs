594874866e78f47a61a0309643c39d4836e5d36c
Pierre Fenoll <pierrefenoll@gmail.com>
2025-03-29 00:54:51 +0100

    find_toml_from_env
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- cargo-green/src/lockfile.rs
+++ cargo-green/src/lockfile.rs
@@ -1,8 +1,9 @@
-use std::env;
+use std::{env, process::Output};
 
 use anyhow::{bail, Result};
 use camino::{Utf8Path, Utf8PathBuf};
 use cargo_lock::{Lockfile, Package, SourceId};
+use pico_args::Arguments;
 use serde::Deserialize;
 use tokio::process::Command;
 
@@ -36,6 +37,12 @@ pub(crate) async fn find_lockfile() -> Result<Utf8PathBuf> {
     Ok(manifest_path.with_extension("lock"))
 }
 
+fn find_toml_from_env() -> Result<Option<Utf8PathBuf>> {
+    let mut args = Arguments::from_env();
+    Ok(args.opt_value_from_str("--manifest-path")?.map(|x: String| Into::<Utf8PathBuf>::into(x)))
+}
+
+// Returns Cargo.toml
 // https://doc.rust-lang.org/cargo/commands/cargo-locate-project.html
 // https://github.com/rust-lang/cargo/blob/3e96f1a28e47d4fd0f354b3a067d6322a8730cb6/src/bin/cargo/commands/locate_project.rs#L29
 async fn cargo_locate_project(at_workspace: bool) -> Result<Utf8PathBuf> {
@@ -43,13 +50,17 @@ async fn cargo_locate_project(at_workspace: bool) -> Result<Utf8PathBuf> {
     cmd.kill_on_drop(true);
 
     cmd.arg("locate-project");
+    if let Some(manifest_path) = find_toml_from_env()? {
+        cmd.arg("--manifest-path");
+        cmd.arg(manifest_path);
+    }
     if at_workspace {
         cmd.arg("--workspace");
     }
 
-    let output = cmd.output().await?;
-    if !output.stderr.is_empty() {
-        bail!("{} failed: {:?}", cmd.show(), output.stderr)
+    let Output { stderr, stdout, status } = cmd.output().await?;
+    if !status.success() || !stderr.is_empty() {
+        bail!("{} failed: {stderr:?}", cmd.show())
     }
 
     #[derive(Debug, Deserialize)]
@@ -57,6 +68,6 @@ async fn cargo_locate_project(at_workspace: bool) -> Result<Utf8PathBuf> {
         root: Utf8PathBuf,
     }
 
-    let Located { root } = serde_json::from_slice(&output.stdout)?;
+    let Located { root } = serde_json::from_slice(&stdout)?;
     Ok(root)
 }
