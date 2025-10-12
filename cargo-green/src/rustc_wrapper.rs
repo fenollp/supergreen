366bb0a1106c89929cae42a077fd38485fd1d653
Pierre Fenoll <pierrefenoll@gmail.com>
2025-10-12 12:58:46 +0200

    refacto: move assemble_build_dependencies under Md
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- cargo-green/src/rustc_wrapper.rs
+++ cargo-green/src/rustc_wrapper.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeMap, HashMap},
+    collections::BTreeMap,
     env,
     fs::{self},
     future::Future,
@@ -7,7 +7,6 @@ use std::{
 
 use anyhow::{anyhow, bail, Result};
 use camino::{Utf8Path, Utf8PathBuf};
-use indexmap::IndexSet;
 use log::{debug, error, info, trace, warn};
 use tokio::process::Command;
 
@@ -18,7 +17,7 @@ use crate::{
     ext::CommandExt,
     green::Green,
     logging::{self, crate_type_for_logging, maybe_log},
-    md::{BuildContext, Md},
+    md::{BuildContext, Md, NamedMount},
     pwd,
     runner::Runner,
     rustc_arguments::{as_rustc, RustcArgs},
@@ -355,8 +354,7 @@ async fn do_wrap_rustc(
     info!("loading {} build contexts", md.contexts.len());
 
     let (mounts, mds) =
-        assemble_build_dependencies(&mut md, &crate_type, &emit, externs, &target_path)?;
-
+        md.assemble_build_dependencies(&crate_type, &emit, externs, &target_path)?;
     for NamedMount { name, src, dst } in mounts {
         rustc_block.push_str(&format!("  --mount=from={name},dst={dst},source={src} \\\n"));
     }
@@ -502,113 +500,6 @@ async fn do_wrap_rustc(
     Ok(())
 }
 
-#[derive(Debug)]
-struct NamedMount {
-    name: Stage,
-    src: Utf8PathBuf,
-    dst: Utf8PathBuf,
-}
-
-fn assemble_build_dependencies(
-    md: &mut Md,
-    crate_type: &str,
-    emit: &str,
-    externs: IndexSet<String>,
-    target_path: &Utf8Path,
-) -> Result<(Vec<NamedMount>, Vec<Md>)> {
-    let mut mds = HashMap::<Utf8PathBuf, Md>::new(); // A file cache
-
-    let md_pather = |part: &str| target_path.join(format!("{part}.toml"));
-
-    // https://github.com/rust-lang/cargo/issues/12059#issuecomment-1537457492
-    //   https://github.com/rust-lang/rust/issues/63012 : Tracking issue for -Z binary-dep-depinfo
-    let mut all_externs = IndexSet::new();
-
-    let ext = match crate_type {
-        "lib" => "rmeta".to_owned(),
-        "bin" | "rlib" | "test" | "proc-macro" => "rlib".to_owned(),
-        _ => bail!("BUG: unexpected crate-type: '{crate_type}'"),
-    };
-    // https://rustc-dev-guide.rust-lang.org/backend/libs-and-metadata.html#rmeta
-    // > [rmeta] is created if the --emit=metadata CLI option is used.
-    let ext = if emit.contains("metadata") { "rmeta".to_owned() } else { ext };
-
-    for xtern in externs {
-        trace!("❯ extern {xtern}");
-        all_externs.insert(xtern.clone());
-
-        if !xtern.starts_with("lib") {
-            bail!("BUG: expected extern to match ^lib: {xtern}")
-        }
-        let xtern = xtern.strip_prefix("lib").expect("PROOF: ~ ^lib");
-        let xtern = if xtern.ends_with(".rlib") {
-            xtern.strip_suffix(".rlib")
-        } else if xtern.ends_with(".rmeta") {
-            xtern.strip_suffix(".rmeta")
-        } else if xtern.ends_with(".so") {
-            xtern.strip_suffix(".so")
-        } else {
-            bail!("BUG: cargo gave unexpected extern: {xtern:?}")
-        }
-        .expect("PROOF: all cases match");
-        trace!("❯ short extern {xtern}");
-        md.short_externs.insert(xtern.to_owned());
-
-        let extern_md = md_pather(xtern);
-        info!("checking (RO) extern's externs {extern_md}");
-        let extern_md = get_or_read(&mut mds, &extern_md)?;
-        for transitive in extern_md.short_externs {
-            let guard_md = get_or_read(&mut mds, &md_pather(&transitive))?;
-            let ext = if guard_md.is_proc_macro { "so" } else { &ext };
-
-            trace!("❯ extern lib{transitive}.{ext}");
-            all_externs.insert(format!("lib{transitive}.{ext}"));
-
-            trace!("❯ short extern {transitive}");
-            md.short_externs.insert(transitive);
-        }
-    }
-
-    let mut mounts = Vec::with_capacity(all_externs.len());
-    let extern_mds_and_paths = all_externs
-        .into_iter()
-        .map(|xtern| {
-            let Some((extern_md_path, xtern_stage)) = toml_path_and_stage(&xtern, target_path)
-            else {
-                bail!("Unexpected extern name format: {xtern}")
-            };
-            let mount = NamedMount {
-                name: xtern_stage,
-                src: format!("/{xtern}").into(),
-                dst: target_path.join("deps").join(xtern),
-            };
-            mounts.push(mount);
-
-            let extern_md = get_or_read(&mut mds, &extern_md_path)?;
-            Ok((extern_md_path, extern_md))
-        })
-        .collect::<Result<Vec<_>>>()?;
-
-    let extern_md_paths = md.sort_deps(extern_mds_and_paths)?;
-    info!("extern_md_paths: {} {extern_md_paths:?}", extern_md_paths.len());
-
-    let mds = extern_md_paths
-        .into_iter()
-        .map(|extern_md_path| get_or_read(&mut mds, &extern_md_path))
-        .collect::<Result<Vec<_>>>()?;
-
-    Ok((mounts, mds))
-}
-
-fn get_or_read(mds: &mut HashMap<Utf8PathBuf, Md>, path: &Utf8Path) -> Result<Md> {
-    if let Some(md) = mds.get(path) {
-        return Ok(md.clone());
-    }
-    let md = Md::from_file(path)?;
-    let _ = mds.insert(path.to_path_buf(), md.clone());
-    Ok(md)
-}
-
 fn fmap_env((var, val): (String, String), buildrs: bool) -> Option<(String, String)> {
     let (pass, skip, only_buildrs) = pass_env(&var);
     if pass || (buildrs && only_buildrs) {
@@ -700,40 +591,6 @@ fn safeify(val: String) -> String {
 #[test]
 fn test_safeify() {
     assert_eq!(safeify("$VAR=val".to_owned()), "\"\\$VAR=val\"".to_owned());
-}
-
-#[test]
-fn toml_path_and_stage_for_rlib() {
-    let xtern = "libstrsim-8ed1051e7e58e636.rlib";
-    let res = toml_path_and_stage(xtern, "./target/path".into()).unwrap();
-    assert_eq!(res.0, "./target/path/strsim-8ed1051e7e58e636.toml".to_owned());
-    assert_eq!(res.1, "out-8ed1051e7e58e636".try_into().unwrap());
-}
-
-#[test]
-fn toml_path_and_stage_for_libc() {
-    let xtern = "liblibc-c53783e3f8edcfe4.rmeta";
-    let res = toml_path_and_stage(xtern, "./target/path".into()).unwrap();
-    assert_eq!(res.0, "./target/path/libc-c53783e3f8edcfe4.toml".to_owned());
-    assert_eq!(res.1, "out-c53783e3f8edcfe4".try_into().unwrap());
-}
-
-#[test]
-fn toml_path_and_stage_for_weird_extension() {
-    let xtern = "libthing-131283e3f8edcfe4.a.2.c";
-    let res = toml_path_and_stage(xtern, "./target/path".into()).unwrap();
-    assert_eq!(res.0, "./target/path/thing-131283e3f8edcfe4.toml".to_owned());
-    assert_eq!(res.1, "out-131283e3f8edcfe4".try_into().unwrap());
-}
-
-#[must_use]
-fn toml_path_and_stage(xtern: &str, target_path: &Utf8Path) -> Option<(Utf8PathBuf, Stage)> {
-    // TODO: drop stripping ^lib
-    assert!(xtern.starts_with("lib"), "BUG: unexpected xtern format: {xtern}");
-    let pa = xtern.strip_prefix("lib").and_then(|x| x.split_once('.')).map(|(x, _)| x);
-    let st = pa.and_then(|x| x.split_once('-')).map(|(_, x)| Stage::output(x).unwrap());
-    let pa = pa.map(|x| target_path.join(format!("{x}.toml")));
-    pa.zip(st)
 }
 
 #[test]
