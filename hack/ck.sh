e82ae06c03d19663b6bc81179982aa39932a7cf6
Pierre Fenoll <pierrefenoll@gmail.com>
2025-11-02 21:14:53 +0100

    ci: fix stable for CLIs
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- hack/ck.sh
+++ hack/ck.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env -S bash -eu
 set -o pipefail
 
+stable=1.90.0 # Closest to latest stable, as official Rust images availability permits (TODO: use rustup when image isn't yet available)
+fixed=1.86.0 # Some fixed rustc version
+
 
 jobdef() {
     local name=$1; shift
@@ -118,7 +121,7 @@ cat <<EOF
       run: |
         ! grep -C20 -F ' >>> ' \$CARGOGREEN_LOG_PATH
 
-    - if: \${{ always() && env.CARGOGREEN_FINAL_PATH != '' && matrix.toolchain != 'stable' }}
+    - if: \${{ always() && env.CARGOGREEN_FINAL_PATH != '' && matrix.toolchain != '$stable' }}
       name: 🌀 Maybe show final path diff
       run: |
         case "\$GITHUB_JOB" in
