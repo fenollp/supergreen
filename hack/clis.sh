d2af5eb3d55d5bd2eac63a4a5695147783b42743
Pierre Fenoll <pierrefenoll@gmail.com>
2026-03-18 23:47:45 +0100

    ci: disable finalpathcomments when running locally
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- hack/clis.sh
+++ hack/clis.sh
@@ -595,10 +595,8 @@ envvars+=(CARGOGREEN_LOG_PATH="$tmplogs")
 envvars+=(CARGO_TARGET_DIR="$tmptrgt")
 if [[ "$final" = '1' ]]; then
   envvars+=(CARGOGREEN_FINAL_PATH=recipes/$name_at_version.Dockerfile)
-  envvars+=(CARGOGREEN_EXPERIMENT=finalpathnonprimary,finalpathcomments)
+  envvars+=(CARGOGREEN_EXPERIMENT=finalpathnonprimary) #,finalpathcomments)
 fi
-# envvars+=(CARGOGREEN_SYNTAX_IMAGE=docker-image://docker.io/docker/dockerfile:1@sha256:4c68376a702446fc3c79af22de146a148bc3367e73c25a5803d453b6b3f722fb)
-# envvars+=(CARGOGREEN_BASE_IMAGE=docker-image://docker.io/library/rust:1.86.0-slim@sha256:3f391b0678a6e0c88fd26f13e399c9c515ac47354e3cadfee7daee3b21651a4f)
 as_env "$name_at_version"
 send \
   'until' '[[' -f "$tmpgooo".installed ']];' 'do' sleep '.1;' 'done' '&&' rm "$tmpgooo".* \
