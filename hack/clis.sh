3b566b649ff212aaed6b27dbb942e35aa30b7f7c
Pierre Fenoll <pierrefenoll@gmail.com>
2026-04-30 14:12:41 +0200

    feat(ci): add zed
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- hack/clis.sh
+++ hack/clis.sh
@@ -115,7 +115,7 @@ declare -a nvs nvs_args toolchain
 #     = note: required for the cast from `Box<fn(&Session) -> Box<...> {make_miri_codegen_backend}>` to `Box<dyn FnOnce(&Options, &Target) -> Box<dyn CodegenBackend> + Send>`
 #     = note: the full name for the type has been written to '/target/release/deps/miri-e9f47534ee52cbf9.long-type-13241406945400517937.txt'
 #     = note: consider using `--verbose` to print the full type name to the console
-((i+=1)); nvs[i]=zed@main;                    oks[i]=ko; nvs_args[i]='--git https://github.com/zed-industries/zed.git --tag=v0.233.10';
+((i+=1)); nvs[i]=zed@main;                    oks[i]=ok; nvs_args[i]='--git https://github.com/zed-industries/zed.git --tag=v1.0.0';
 # In file included from /home/pete/.cargo/registry/src/index.crates.io/tree-sitter-0.26.8/src/lib.c:13:
 # /home/pete/.cargo/registry/src/index.crates.io/tree-sitter-0.26.8/src/./wasm_store.c:16:10: fatal error: wasm.h: No such file or directory
 #    16 | #include <wasm.h>
@@ -253,7 +253,7 @@ as_env() {
     rublk@*) envvars+=(CARGOGREEN_ADD_APT='libclang-dev') ;;
     sccache@*) envvars+=(CARGOGREEN_ADD_APT='"libssl-dev(>=3.5)",pkg-config,zlib1g-dev') ;;
     torrust-index@*) envvars+=(CARGOGREEN_ADD_APT='"libssl-dev(>=3.5)",pkg-config,zlib1g-dev') ;;
-    zed@*) envvars+=(CARGOGREEN_ADD_APT='build-essential,clang,cmake,curl,elfutils,g++,gcc,gettext-base,git,jq,libasound2-dev,libfontconfig-dev,libgit2-dev,libglib2.0-dev,libsqlite3-dev,libssl-dev=3.5.5-1~deb13u2,libva-dev,libvulkan1,libwayland-dev,libx11-xcb-dev,libxkbcommon-x11-dev,libzstd-dev,lld,llvm,make,musl-dev,musl-tools,pipewire,xdg-desktop-portal') ;; # From https://github.com/zed-industries/zed/blob/v0.233.10/script/linux#L25-L52
+    zed@*) envvars+=(CARGOGREEN_ADD_APT='build-essential,clang,cmake,curl,elfutils,g++,gcc,gettext-base,git,jq,libasound2-dev,libfontconfig-dev,libgit2-dev,libglib2.0-dev,libsqlite3-dev,"libssl-dev(>=3.5)",libva-dev,libvulkan1,libwayland-dev,libx11-xcb-dev,libxkbcommon-x11-dev,libzstd-dev,lld,llvm,make,musl-dev,musl-tools,pipewire,xdg-desktop-portal') ;; # From https://github.com/zed-industries/zed/blob/v0.233.10/script/linux#L25-L52
     *) ;;
   esac
 
