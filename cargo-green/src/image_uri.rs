02ff0c1c82b93b466ad1a43f7cc6ca87b904da76
Pierre Fenoll <pierrefenoll@gmail.com>
2026-05-17 21:22:24 +0200

    refacto(build): move ImageUri methods to that module
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- cargo-green/src/image_uri.rs
+++ cargo-green/src/image_uri.rs
@@ -1,7 +1,18 @@
-use std::sync::LazyLock;
+use std::{error::Error as StdError, sync::LazyLock, time::Duration};
 
-use anyhow::{bail, Error, Result};
+use anyhow::{anyhow, bail, Error, Result};
+use camino::Utf8Path;
+use log::info;
 use nutype::nutype;
+use reqwest::Client as ReqwestClient;
+use serde::Deserialize;
+
+use crate::{
+    du::lock_from_builder_cache,
+    ext::CommandExt,
+    green::Green,
+    runner::{Runner, DOCKER_HOST},
+};
 
 /// Default BuildKit syntax: `docker-image://docker.io/docker/dockerfile:1`
 pub(crate) static SYNTAX_IMAGE: LazyLock<ImageUri> =
@@ -305,4 +316,147 @@ fn imageuri_ipv6() {
         "sha256:27086352fd5e1907ea2b934eb1023f217c5ae087992eb59fde121dce9c9ff21e"
     );
     assert_eq!(img.host(), "[2001:db8:1f70::999:de8:7648:6e8]:100");
+}
+
+impl Green {
+    /// Read digest from builder cache, then maybe from default cache.
+    ///
+    /// No-op for an already locked image URI.
+    ///
+    /// Goal is to have a completely offline mode by default, after a `cargo green fetch`.
+    pub(crate) async fn maybe_lock_image(&self, img: &ImageUri) -> Result<ImageUri> {
+        if img.locked() {
+            return Ok(img.to_owned());
+        }
+        let errer = |e| anyhow!("Failed locking {img}: {e}");
+        if let Some(locked) = self.maybe_lock_from_builder_cache(img).await.map_err(errer)? {
+            return Ok(locked);
+        }
+        if let Some(locked) = self.maybe_lock_from_image_cache(img).await.map_err(errer)? {
+            return Ok(locked);
+        }
+        Ok(img.to_owned())
+    }
+
+    /// Reads from builder build cache if any, and falls back to image cache.
+    ///
+    /// <https://docs.docker.com/reference/cli/docker/buildx/imagetools/inspect/>
+    ///
+    /// ```text
+    /// docker buildx imagetools inspect --format='{{json .Manifest.Digest}}' img.noscheme()
+    /// # Only fetches remote though, and takes ages compared to fetch_digest!
+    /// ```
+    /// See [Getting an image's digest fast, within a docker-container builder](https://github.com/docker/buildx/discussions/3363)
+    async fn maybe_lock_from_builder_cache(&self, img: &ImageUri) -> Result<Option<ImageUri>> {
+        let cached = self.images_in_builder_cache().await?;
+        Ok(lock_from_builder_cache(img.noscheme(), cached).map(|digest| img.lock(digest)))
+    }
+
+    /// If given an un-pinned image URI, query local image cache for its digest.
+    ///
+    /// Returns the given URI, along with its digest if one was found.
+    ///
+    /// <https://docs.docker.com/dhi/core-concepts/digests/>
+    async fn maybe_lock_from_image_cache(&self, img: &ImageUri) -> Result<Option<ImageUri>> {
+        if self.runner.is_none() {
+            info!("Skipping inspecting image cache (runner:{})", self.runner);
+            return Ok(None);
+        }
+        let mut cmd = self.cmd()?;
+        cmd.arg("inspect").arg("--format={{index .RepoDigests 0}}").arg(img.noscheme());
+
+        let (succeeded, stdout, stderr) = cmd.exec().await?;
+        if !succeeded {
+            let stderr = String::from_utf8_lossy(&stderr);
+            if stderr.to_lowercase().contains("no such object") {
+                return Ok(None);
+            }
+
+            let mut help = "";
+            if stderr.to_lowercase().contains(" executable file not found in ")
+                && self.runner_envs.contains_key(DOCKER_HOST)
+            {
+                // TODO: find actual solutions to 'executable file not found in $PATH'
+                // error during connect: Get "http://docker.example.com/v1.51/containers/docker.io/docker/dockerfile:1/json": exec: "ssh": executable file not found in $PATH
+                // error during connect: Get "http://docker.example.com/v1.51/containers/json": command [ssh -o ConnectTimeout=30 -T -- gol docker system dial-stdio] has exited with exit status 127, make sure the URL is valid, and Docker 18.09 or later is installed on the remote host: stderr=bash: line 1: docker: command not found
+                help = r#"
+Maybe have a look at
+  https://stackoverflow.com/a/79474080/1418165
+  https://github.com/docker/for-mac/issues/4382#issuecomment-603031242
+"#
+                .trim();
+            }
+            bail!("BUG: failed to inspect image cache: {stderr}{help}")
+        }
+
+        Ok(String::from_utf8_lossy(&stdout)
+            .lines()
+            .next()
+            .and_then(|line| ImageUri::try_new(format!("docker-image://{line}")).ok())
+            // NOTE: `inspect` does not keep tag: host/dir/name@sha256:digest (no :tag@)
+            .map(|digested| img.lock(digested.digest())))
+    }
+}
+
+/// If given an un-pinned image URI, query remote image API for its digest.
+///
+/// No-op for an already locked image URI.
+pub(crate) async fn fetch_digest(runner: &Runner, img: &ImageUri) -> Result<ImageUri> {
+    // TODO: add+impl traits on runner (fetch_digest, do_build, ..) Maybe on Green?
+    if runner.is_none() {
+        info!("Skipping fetching image digest (runner:{runner})");
+        return Ok(img.to_owned());
+    }
+
+    if img.locked() {
+        return Ok(img.to_owned());
+    }
+
+    async fn actual(img: &ImageUri) -> Result<ImageUri> {
+        let (path, tag) = img.path_and_tag();
+        let (ns, slug) = match Utf8Path::new(path).iter().collect::<Vec<_>>()[..] {
+            ["docker.io", ns, slug] => (ns, slug),
+            _ => bail!("BUG: unhandled registry {img:?}"),
+        };
+
+        let domain = "registry.hub.docker.com";
+        let (client, req) = ReqwestClient::builder()
+            .connect_timeout(Duration::from_secs(4))
+            .build()
+            .map_err(|e| anyhow!("HTTP client's config/TLS failed: {e}"))?
+            .get(format!("https://{domain}/v2/repositories/{ns}/{slug}/tags/{tag}"))
+            .build_split();
+        let req = req.map_err(|e| {
+            // e.source(): try to be a bit more helpful than just "error sending request for url"
+            anyhow!("Failed to build a request against {domain}: {e} ({:?})", e.source())
+        })?;
+
+        info!("GETing {}", req.url());
+        eprintln!("GETing {}", req.url());
+        assert!(req.body().is_none());
+        assert!(req.headers().is_empty());
+
+        let txt = client
+            .execute(req)
+            .await
+            .map_err(|e| anyhow!("Failed to reach {domain}'s registry: {e}"))?
+            .text()
+            .await
+            .map_err(|e| anyhow!("Failed to read response from {domain} registry: {e}"))?;
+
+        #[derive(Deserialize)]
+        struct RegistryResponse {
+            digest: String,
+        }
+        let RegistryResponse { digest } = serde_json::from_str(&txt)
+            // NOTE: library images can take a few days to appear, after a Rust release:
+            // Error: Failed to decode response from registry: missing field `digest` at line 1 column 130
+            // {"message":"httperror 404: tag '1.89.0-slim' not found","errinfo":{"namespace":"library","repository":"rust","tag":"1.89.0-slim"}}
+            .map_err(|e| anyhow!("Failed to decode response from registry: {e}\n{txt}"))?;
+        // digest ~ sha256:..
+
+        Ok(img.lock(&digest))
+    }
+
+    actual(img).await.map_err(|e| anyhow!("Failed getting digest for {img}: {e}"))
 }
