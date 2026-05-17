ca00639c0cad0dff43f7192ceb0f2c81237fd659
Pierre Fenoll <pierrefenoll@gmail.com>
2026-05-17 20:12:23 +0200

    refacto(cache): introduce cache/ dir
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- cargo-green/src/cache/mod.rs
+++ cargo-green/src/cache/mod.rs
@@ -66,17 +66,17 @@ macro_rules! ENV_CACHE_TO_IMAGES {
 #[serde(default)]
 #[serde(deny_unknown_fields)]
 pub(crate) struct Cache {
-    #[doc = include_str!(concat!("../docs/",ENV_CACHE_FROM_IMAGES!(),".md"))]
+    #[doc = include_str!(concat!("../../docs/",ENV_CACHE_FROM_IMAGES!(),".md"))]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     #[serde(rename = "cache-from-images")]
     pub(crate) from_images: Vec<ImageUri>,
 
-    #[doc = include_str!(concat!("../docs/",ENV_CACHE_TO_IMAGES!(),".md"))]
+    #[doc = include_str!(concat!("../../docs/",ENV_CACHE_TO_IMAGES!(),".md"))]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     #[serde(rename = "cache-to-images")]
     pub(crate) to_images: Vec<ImageUri>,
 
-    #[doc = include_str!(concat!("../docs/",ENV_CACHE_IMAGES!(),".md"))]
+    #[doc = include_str!(concat!("../../docs/",ENV_CACHE_IMAGES!(),".md"))]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     #[serde(rename = "cache-images")]
     pub(crate) images: Vec<ImageUri>,
