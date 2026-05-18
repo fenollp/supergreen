f5c0a602d1961fa0e6260cc94cbe6eaa992540c3
Pierre Fenoll <pierrefenoll@gmail.com>
2026-05-18 16:06:39 +0200

    refacto: introduce wrap modules
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

---
+++ cargo-green/src/wrap/mds.rs
@@ -0,0 +1,180 @@
+use std::{collections::HashSet, env};
+
+use anyhow::{anyhow, Result};
+use camino::Utf8Path;
+use log::{debug, info, warn};
+
+use crate::{
+    build::{Effects, ERRCODE, STDERR, STDOUT},
+    green::Green,
+    md::Md,
+    stage::Stage,
+    target_dir::virtual_target_dir,
+    wrap::{
+        build_script::{exe_dance, is_buildrs_executable},
+        envs::{fmap_env, rewrite_env},
+    },
+    ENV,
+};
+
+impl Md {
+    pub(crate) fn call_block(
+        &mut self,
+        (stage, mut block): (&Stage, String),
+        crate_name: Option<&str>,
+        cargo_home: &Utf8Path,
+        green_set_envs: &[String],
+        call: &str,
+        (out_stage, out_dir): (&Stage, Option<&Utf8Path>),
+    ) -> Result<()> {
+        let mut first = true;
+        let mut push = |block: &mut String, var: &str, val: &String| -> Result<_> {
+            let val = rewrite_env(val, cargo_home)?;
+            block.push_str(&format!("    {} {var}={val} \\\n", if first { "env" } else { "   " }));
+            first = false;
+            Ok(())
+        };
+
+        let mut set: HashSet<_> =
+            ["CARGO".to_owned(), "RUSTC".to_owned(), "RUSTUP_TOOLCHAIN".to_owned()].into();
+
+        let mut vars = env::vars().collect::<Vec<_>>();
+        vars.sort_by(|(a, _), (b, _)| a.cmp(b));
+        for (var, val) in vars.into_iter().filter_map(|kv| fmap_env(kv, self.buildrs)) {
+            if set.contains(&var) {
+                continue;
+            }
+            push(&mut block, &var, &val)?;
+            set.insert(var.clone());
+        }
+        block.push_str(&format!("        {}=1 \\\n", ENV!()));
+
+        for (var, val) in &self.set_envs {
+            if set.contains(var) {
+                continue;
+            }
+            warn!("setting rustc-env: ${var}={val:?}");
+            push(&mut block, var, val)?;
+            set.insert(var.to_owned());
+        }
+
+        for var in green_set_envs {
+            if set.contains(var) {
+                continue;
+            }
+            if let Ok(val) = env::var(var) {
+                warn!("passing ${var}={val:?} env through");
+                push(&mut block, var, &val)?;
+                set.insert(var.to_owned());
+            }
+        }
+
+        // TODO: keep only paths that we explicitly mount or copy
+        if false {
+            // https://github.com/maelstrom-software/maelstrom/blob/ef90f8a990722352e55ef1a2f219ef0fc77e7c8c/crates/maelstrom-util/src/elf.rs#L4
+            for var in ["PATH", "DYLD_FALLBACK_LIBRARY_PATH", "LD_LIBRARY_PATH", "LIBPATH"] {
+                let Ok(val) = env::var(var) else { continue };
+                if set.contains(var) {
+                    continue;
+                }
+                debug!("system env set (skipped): ${var}={val:?}");
+                push(&mut block, var, &val)?;
+            }
+        }
+
+        let out_dir = out_dir.map(virtual_target_dir).unwrap_or(".".into());
+        // TODO: let out_dir = out_dir.map(|_| "$OLDPWD").unwrap_or("$PWD"); whence  https://github.com/moby/buildkit/issues/6698  [frontend] $OLDPWD is unset (after >1 WORKDIR layers)
+
+        block.push_str(&format!("      {call} \\\n"));
+        block.push_str(&format!("        1>          {out_dir}/{out_stage}-{STDOUT} \\\n"));
+        block.push_str(&format!("        2>          {out_dir}/{out_stage}-{STDERR} \\\n"));
+        block.push_str(&format!("        || echo $? >{out_dir}/{out_stage}-{ERRCODE}\\\n"));
+
+        if let Some(crate_name) = crate_name {
+            if is_buildrs_executable(crate_name) {
+                block.push_str(&exe_dance(self.this(), crate_name, &out_dir));
+                block.push_str(&format!(" || echo $? >{out_dir}/{out_stage}-{ERRCODE} \\\n"));
+            }
+        }
+
+        // TODO: [`COPY --rewrite-timestamp ...` to apply SOURCE_DATE_EPOCH build arg value to the timestamps of the files](https://github.com/moby/buildkit/issues/6348)
+        let pattern = if self.buildrs { "*".to_owned() } else { format!("*-{}*", self.this()) };
+        block.push_str(&format!("  ; find {out_dir}/{pattern} -exec touch --no-dereference --date=@$SOURCE_DATE_EPOCH '{{}}' + \\\n"));
+        block.push_str(&format!(" || echo $? >{out_dir}/{out_stage}-{ERRCODE}\n"));
+
+        self.push_block(stage, &block);
+        Ok(())
+    }
+
+    /// TODO? in Dockerfile, when using outputs:
+    /// => skip the COPY (--mount=from=out-08c4d63ed4366a99) use the stage directly
+    pub(crate) fn out_block(
+        &mut self,
+        stage: &Stage,
+        prev: &Stage,
+        out_dir: &Utf8Path,
+        buildrs: bool,
+    ) {
+        let mut block = format!("FROM scratch AS {stage}\n");
+        let out_dir = virtual_target_dir(out_dir);
+        if buildrs {
+            block.push_str(&format!("COPY --link --from={prev} {out_dir} /\n"));
+        } else {
+            let mdid = self.this();
+            block.push_str(&format!("COPY --link --from={prev} {out_dir}/*-{mdid}* /\n"));
+        }
+        self.push_block(stage, &block);
+    }
+
+    pub(crate) async fn do_build(
+        &mut self,
+        green: &Green,
+        containerfile_path: &Utf8Path,
+        stage: &Stage,
+        out_dir: &Utf8Path,
+        target_path: &Utf8Path,
+    ) -> Result<()> {
+        let (call, envs, Effects { written, stdout, stderr, cargo_rustc_env }, built) =
+            green.build_out(containerfile_path, stage, &self.contexts, out_dir).await;
+
+        green
+            .maybe_write_final_path(containerfile_path, &self.contexts, &call, &envs)
+            .map_err(|e| anyhow!("Failed producing final path: {e}"))?;
+
+        let md_path = self.this().path(target_path);
+
+        if !written.is_empty()
+            || !stdout.is_empty()
+            || !stderr.is_empty()
+            || !cargo_rustc_env.is_empty()
+        {
+            self.writes = written;
+            self.stdout = stdout;
+            self.stderr = stderr;
+            // self.cargo_rustc_env = cargo_rustc_env;/////
+            info!("re-opening (RW) crate's md {md_path}");
+            self.write_to(&md_path)?;
+        }
+
+        let final_stage = format!(
+            "FROM scratch\n{}\n",
+            self.writes
+                .iter()
+                .filter_map(|f| f.file_name())
+                .filter(|f| !f.ends_with(".d"))
+                .filter(|f| f != &format!("{stage}-{STDOUT}"))
+                .filter(|f| f != &format!("{stage}-{STDERR}"))
+                .filter(|f| f != &format!("{stage}-{ERRCODE}"))
+                .map(|f| (f, f.replace(&format!("-{}", self.this()), "")))
+                .map(|(src, dst)| format!("COPY --link --from={stage} /{src} /{dst}"))
+                .collect::<Vec<_>>()
+                .join("\n")
+        );
+
+        green
+            .maybe_append_to_final_path(&md_path, final_stage)
+            .map_err(|e| anyhow!("Failed finishing final path: {e}"))?;
+
+        built
+    }
+}
---
+++ cargo-green/src/wrap/rustc.rs
@@ -0,0 +1,207 @@
+use std::{
+    env,
+    fs::{self},
+    future::Future,
+};
+
+use anyhow::{anyhow, bail, Result};
+use camino::{Utf8Path, Utf8PathBuf};
+use log::{error, info, warn};
+
+use crate::{
+    base_image::rewrite_cargo_home,
+    checkouts,
+    cratesio::{self, rewrite_cratesio_index},
+    dirs::pwd,
+    green::Green,
+    logging::{self},
+    md::{BuildContext, Md, MountExtern, NamedMount},
+    relative,
+    rustc_arguments::{as_rustc, RustcArgs},
+    stage::{AsStage, Stage, RST, RUST},
+    target_dir::{virtual_target_dir, virtual_target_dir_str},
+    wrap::{build_script::is_buildrs_executable, call_config, envs::safeify},
+    ENV, PKG, VSN,
+};
+
+pub(crate) async fn wrap_rustc(
+    green: Green,
+    arguments: Vec<String>,
+    fallback: impl Future<Output = Result<()>>,
+) -> Result<()> {
+    assert!(env::var_os(ENV!()).is_none(), "It's turtles all the way down!");
+    env::set_var(ENV!(), "1");
+
+    if green.runner.is_none() {
+        return fallback.await;
+    }
+
+    let pwd = pwd();
+
+    let out_dir_var = env::var("OUT_DIR").ok().map(Utf8PathBuf::from);
+
+    let (st @ RustcArgs { mdid, .. }, args) = as_rustc(&pwd, &arguments, out_dir_var.as_deref())?;
+
+    let (crate_name, pkg_name, pkg_version, pkg_manifest_dir) = call_config();
+
+    let buildrs = crate_name.as_deref().map(is_buildrs_executable).unwrap_or_default();
+    let kind = if buildrs { 'X' } else { 'N' }; // building buildrs eXe or Normal
+    let full_pkg_id = format!("{kind} {pkg_name} {pkg_version} {mdid}");
+
+    logging::setup(&full_pkg_id);
+
+    info!("{PKG}@{VSN} original args: {arguments:?} pwd={pwd} st={st:?} green={green:?}");
+
+    do_wrap_rustc(
+        green,
+        crate_name.as_deref(),
+        &pkg_name,
+        &pkg_manifest_dir,
+        Stage::dep(&full_pkg_id.replace(' ', "-"))?,
+        pwd,
+        args,
+        out_dir_var,
+        st,
+    )
+    .await
+    .inspect_err(|e| error!("Error: {e}"))
+}
+
+#[expect(clippy::too_many_arguments)]
+async fn do_wrap_rustc(
+    green: Green,
+    crate_name: Option<&str>,
+    pkg_name: &str,
+    pkg_manifest_dir: &Utf8Path,
+    rustc_stage: Stage,
+    pwd: Utf8PathBuf,
+    args: Vec<String>,
+    out_dir_var: Option<Utf8PathBuf>,
+    RustcArgs { externs, mdid, incremental, input, out_dir, target_path }: RustcArgs,
+) -> Result<()> {
+    let mut md: Md = mdid.into();
+    md.buildrs = crate_name.map(is_buildrs_executable).unwrap_or_default();
+    md.push_block(&RUST, &green.base.image_inline);
+
+    fs::create_dir_all(&out_dir).map_err(|e| anyhow!("Failed to `mkdir -p {out_dir}`: {e}"))?;
+    let incremental = green.incremental().then_some(incremental).flatten();
+    if let Some(ref incremental) = incremental {
+        fs::create_dir_all(incremental)
+            .map_err(|e| anyhow!("Failed to `mkdir -p {incremental}`: {e}"))?;
+    }
+
+    info!("picked {rustc_stage} for {input}");
+
+    let mut rustc_block = format!("FROM {RST} AS {rustc_stage}\n");
+
+    rustc_block.push_str(&format!("WORKDIR {out_dir}\n", out_dir = virtual_target_dir(&out_dir)));
+    let not_a_cratesio_crate = !pwd.starts_with(green.cargo_home.join(cratesio::HOME));
+    if not_a_cratesio_crate {
+        let pwd = virtual_target_dir(&pwd);
+        let pwd = rewrite_cargo_home(&green.cargo_home, pwd.as_str());
+        rustc_block.push_str(&format!("WORKDIR {pwd}\n"));
+    }
+    if let Some(ref incremental) = incremental {
+        rustc_block.push_str(&format!("WORKDIR {incremental}\n"));
+    }
+
+    // TODO: support non-crates.io crates managers + proxies
+    // TODO: use --secret mounts for private deps (and secret direct artifacts)
+    let mut code_stage = if input.starts_with(green.cargo_home.join(cratesio::HOME)) {
+        // Input is of a crate dep (hosted at crates.io)
+        // Let's optimize this case by fetching & caching crate tarball
+
+        cratesio::named_stage(&green.cargo_home, pkg_name, pkg_manifest_dir).await?
+    } else if pkg_manifest_dir.starts_with(green.cargo_home.join(checkouts::HOME)) {
+        // Input is of a git checked out dep
+
+        checkouts::as_stage(&green.cargo_home, pkg_manifest_dir).await?
+    } else if input.is_relative() {
+        // Input is local code
+
+        relative::as_stage(mdid, &pwd).await?
+    } else {
+        bail!("BUG: unhandled input {input:?} ({pkg_manifest_dir})")
+    };
+    md.push_stage(&code_stage);
+    rustc_block.push_str("RUN \\\n");
+    for (src, dst, swappity) in code_stage.mounts() {
+        let name = code_stage.name();
+        let dst = virtual_target_dir(&dst);
+        let src = src.as_deref().map(|src| format!(",source={src}")).unwrap_or_default();
+        let mount = if swappity { format!(",dst={dst}{src}") } else { format!("{src},dst={dst}") };
+        rustc_block.push_str(&format!("  --mount=from={name}{mount} \\\n"));
+    }
+
+    if let Some((name, uri)) = code_stage.context() {
+        info!("loading {name:?}: {uri}");
+        md.contexts = [BuildContext { name, uri }].into();
+        info!("loading 1 build context");
+    }
+
+    let mds = md.assemble_build_dependencies(externs, out_dir_var, &target_path)?;
+    for MountExtern { from, xtern } in md.externs() {
+        let dst = virtual_target_dir(&target_path).join("deps").join(xtern);
+        rustc_block.push_str(&format!("  --mount=from={from},dst={dst},source=/{xtern} \\\n"));
+    }
+    for NamedMount { name, mount } in &md.mounts {
+        rustc_block.push_str(&format!("  --mount=from={name},dst={mount},source=/ \\\n"));
+    }
+
+    let out_stage = Stage::output(mdid)?;
+
+    let call = {
+        let input = rewrite_cratesio_index(input.as_str());
+        let input = rewrite_cargo_home(&green.cargo_home, &input);
+
+        let args = args
+            .into_iter()
+            .map(|ref x| virtual_target_dir_str(x))
+            .map(|arg| safeify(&arg).unwrap())
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        format!("rustc {args} {input}")
+    };
+    md.call_block(
+        (&rustc_stage, rustc_block),
+        crate_name,
+        &green.cargo_home,
+        &green.set_envs,
+        &call,
+        (&out_stage, not_a_cratesio_crate.then_some(&out_dir)),
+    )?;
+
+    let incremental_stage = Stage::incremental(mdid)?;
+    if let Some(ref incremental) = incremental {
+        let mut incremental_block = format!("FROM scratch AS {incremental_stage}\n");
+        incremental_block.push_str(&format!("COPY --link --from={rustc_stage} {incremental} /\n"));
+        md.push_block(&incremental_stage, &incremental_block);
+    }
+
+    md.out_block(&out_stage, &rustc_stage, &out_dir, false);
+
+    let containerfile_path = md.finalize(&green, &target_path, pkg_name, &mds)?;
+
+    // TODO: use tracing instead:
+    // https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/struct.Subscriber.html
+    // https://crates.io/crates/tracing-appender
+    // https://github.com/tugglecore/rust-tracing-primer
+    // TODO: `cargo green -v{N+1} ..` starts a TUI showing colored logs on above `cargo -v{N} ..`
+
+    md.do_build(&green, &containerfile_path, &out_stage, &out_dir, &target_path).await?;
+
+    if let Some(incremental) = incremental {
+        if let (_, _, _, Err(e)) = green
+            .build_out(&containerfile_path, &incremental_stage, &md.contexts, &incremental)
+            .await
+        {
+            warn!("Error building incremental data: {e}");
+            return Err(e);
+        }
+    }
+
+    drop(code_stage); // Some impl cleans up files
+
+    Ok(())
+}
