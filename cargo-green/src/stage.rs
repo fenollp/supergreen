6f555f52b425084276781c40595a29b1b63d7085
Pierre Fenoll <pierrefenoll@gmail.com>
2026-08-21 18:54:19 +0200

    fix stage: please clippy
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- cargo-green/src/stage.rs
+++ cargo-green/src/stage.rs
@@ -13,8 +13,9 @@ pub(crate) static RUST: LazyLock<Stage> = LazyLock::new(|| Stage::new(RST).unwra
 #[test]
 fn rust_stage() {
     assert_eq!(RUST.as_str(), "rust-base");
-    assert_eq!(format!("{RST}"), "rust-base");
+    assert_eq!(RUST.as_str(), RST);
     assert_ne!(format!("{RUST:?}"), "rust-base");
+    assert_ne!(format!("{RUST:#?}"), "rust-base");
 }
 
 #[nutype(
